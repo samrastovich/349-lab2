ad8653169ad6656d728cb08c561eccb9da4f4c9b
TheLazyEngineer <mbryan_379@yahoo.com>
2017-02-03 01:23:08 -0800

    Wrote Strassen, needs debugging; tests.java = test for combQuads

--- MatrixProduct.java
+++ MatrixProduct.java
@@ -34,7 +34,7 @@ public class MatrixProduct {
    } */
 
 
-   public static void matrixAdd(int[][] A, int[][] B, int[][] C, int rowC, int colC) {
+   private static void matrixAdd(int[][] A, int[][] B, int[][] C, int rowC, int colC) {
       int n = A.length;   
       for (int i = 0; i < n; i++) {
          for (int j = 0; j < n; j++) {
@@ -51,31 +51,87 @@ public class MatrixProduct {
 
    private static int[][] matrixProduct_Strassen(int[][] A, int[][] B, int startRowA, int startColA, int startRowB, int startColB, int n) {
       int[][] C = new int[n][n];
-      int[][] s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
+      int[][] s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, p1, p2, p3, p4, p5, p6, p7, c11, c12, c21, c22;
       int newSize = n / 2;
       if (n == 1) {
          C[0][0] = A[startRowA][startColA] * B[startRowB][startColB];
       }
       else {
-         s1 = matrixDiff(B, 1, 2, B, 2, 2); //s1 = B12 - B22
-         //s2 = A11 + A12
-         //s3 = A21 + A22
-         //s4 = B21 + B11
-         //s5 = A11 + A22
-         //s6 = B11 + B22
-         //s7 = A12 - A22
-         //s8 = B21 + B22
-         //s9 = A11 - A21
-         //s10 = B11 + B12
+         s1 = matrixDiff(B, startRowB, startColB + newSize, B, startRowB + newSize, startColB + newSize, newSize); //s1 = B12 - B22
+         s2 = matrixSum(A, startRowA, startColA, A, startRowA, startColA + newSize, newSize); //s2 = A11 + A12
+         s3 = matrixSum(A, startRowA + newSize, startColA, A, startRowA + newSize, startColA + newSize, newSize); //s3 = A21 + A22
+         s4 = matrixSum(B, startRowB + newSize, startColB, B, startRowB, startColB, newSize); //s4 = B21 + B11
+         s5 = matrixSum(A, startRowA, startColA, A, startRowA + newSize, startColA + newSize, newSize); //s5 = A11 + A22
+         s6 = matrixSum(B, startRowB, startColB, B, startRowB + newSize, startColB + newSize, newSize); //s6 = B11 + B22
+         s7 = matrixDiff(A, startRowA, startColA + newSize, A, startRowA + newSize, startColA + newSize, newSize); //s7 = A12 - A22
+         s8 = matrixSum(B, startRowB + newSize, startColB, B, startRowB + newSize, startColB + newSize, newSize); //s8 = B21 + B22
+         s9 = matrixDiff(A, startRowA, startColA, A, startRowA + newSize, startColA, newSize); //s9 = A11 - A21
+         s10 = matrixSum(B, startRowB, startColB, B, startRowB, startColB + newSize, newSize); //s10 = B11 + B12
+
+         p1 = matrixProduct_Strassen(A, s1, startRowA, startColA, 0, 0, newSize); //p1 = a11 * s1
+         p2 = matrixProduct_Strassen(s2, B, 0, 0, startRowB + newSize, startColB + newSize, newSize); //p2 = s2 * b22
+         p3 = matrixProduct_Strassen(s3, B, 0, 0, startRowB, startColB, newSize); //p3 = s3 * b11
+         p4 = matrixProduct_Strassen(A, s4, startRowA + newSize, startColA + newSize, 0, 0, newSize); //p4 = a22 * s4
+         p5 = matrixProduct_Strassen(s5, s6, 0, 0, 0, 0, newSize); //p5 = s5 * s6
+         p6 = matrixProduct_Strassen(s7, s8, 0, 0, 0, 0, newSize); //p6 = s7 * s8
+         p7 = matrixProduct_Strassen(s9, s10, 0, 0, 0, 0, newSize); //p7 = s9 * s10
+
+         c11 = matrixSum(matrixDiff(matrixSum(p5, 0, 0, p4, 0, 0, newSize), 0, 0, p2, 0, 0, newSize), 0, 0, p6, 0, 0, newSize); //c11 = p5 + p4 - p2 + p6
+         c12 = matrixSum(p1, 0, 0, p2, 0, 0, newSize); //c12 = p1 + p2
+         c21 = matrixSum(p3, 0, 0, p4, 0, 0, newSize); //c21 = p3 + p4
+         c22 = matrixDiff(matrixDiff(matrixSum(p5, 0, 0, p1, 0, 0, newSize), 0, 0, p3, 0, 0, newSize), 0, 0, p7, 0, 0, newSize); //c22 = p5 + p1 - p3 - p7
+
+         combQuads(C, c11, c12, c21, c22, newSize);
       }
+      return C;
    }
 
-   private static void matrixSum(int[][] A, startRowA, startColA, int[][] B, startRowB, startColB, n) {
-
+   public static int[][] combQuads(int[][] res, int[][] uppLeft, int[][] uppRight, int[][] botLeft, int[][] botRight, int quadSize) { //TESTED: PASSED
+      for (int i = 0; i < quadSize; i++) {
+         for (int j = 0; j < quadSize; j++) {
+            res[i][j] = uppLeft[i][j];
+         }
+      }
+      for (int i = 0; i < quadSize; i++) {
+         for (int j = 0; j < quadSize; j++) {
+            res[i][j + quadSize] = uppRight[i][j];
+         }
+      }
+      for (int i = 0; i < quadSize; i++) {
+         for (int j = 0; j < quadSize; j++) {
+            res[i + quadSize][j] = botLeft[i][j];
+         }
+      }
+      for (int i = 0; i < quadSize; i++) {
+         for (int j = 0; j < quadSize; j++) {
+            res[i + quadSize][j + quadSize] = botRight[i][j];
+         }
+      }
+      return res;
    }
 
-   private static void matrixDiff(int[][] A, startRowA, startColA, int[][] B, startRowB, startColB, n) {
+   private static int[][] matrixSum(int[][] A, int startRowA, int startColA, int[][] B, int startRowB, int startColB, int n) {
+      int[][] result = new int[n][n];
 
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            result[i][j] = A[startRowA + i][startColA + j] + B[startRowB + i][startColB + j];
+         }
+      }
+
+      return result;
+   }
+
+   private static int[][] matrixDiff(int[][] A, int startRowA, int startColA, int[][] B, int startRowB, int startColB, int n) {
+      int[][] result = new int[n][n];
+
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            result[i][j] = A[startRowA + i][startColA + j] - B[startRowB + i][startColB + j];
+         }
+      }
+
+      return result;
    }
 
    private static void checkSize(int[][] A, int[][] B) {
--- tests.java
+++ tests.java
@@ -3,64 +3,64 @@ import java.lang.IllegalArgumentException;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class MatrixWork {
-   public static int[][] matrixProduct(int[][] A, int[][] B) {
-      int[][] C = new int[A.length][B[0].length];
-      if (A[0].length != B.length) {
-         throw new IllegalArgumentException();
-      } 
-      else {   
-         int sum = 0;
-         for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B[0].length; j++) {
-               for (int l = 0; l < A[0].length; l++) {
-                   sum += A[i][l] * B[l][j];
-               }
-               C[i][j] = sum;
-               sum = 0;
-            }
-         }          
-      }
-      return C;
-   }
+public class tests {
    public static void main(String[] args) {
       System.out.println("Enter your file name: ");
       Scanner in = new Scanner(System.in);
       String fileName = in.nextLine();
       try {
-      	in = new Scanner(new File(fileName));
+         in = new Scanner(new File(fileName));
       }
       catch (FileNotFoundException e) {
-      	System.out.println("File does not exist\n");
-      	return;
+         System.out.println("File does not exist\n");
+         return;
       }
-      int[][] A, B, C;
+      int[][] uppLeft, uppRight, botLeft, botRight, C;
       
       int rows = in.nextInt();
       int cols = in.nextInt();
-      A = new int[rows][cols];
+
+      C = new int[rows * 2][cols * 2];
+      uppLeft = new int[rows][cols];
       for (int i = 0; i < rows; i++) {
          for (int j = 0; j < cols; j++) {
-            A[i][j] = in.nextInt();
+            uppLeft[i][j] = in.nextInt();
          }
       }
       
       rows = in.nextInt();
       cols = in.nextInt();
-      B = new int[rows][cols];
+      uppRight = new int[rows][cols];
       for (int i = 0; i < rows; i++) {
          for (int j = 0; j < cols; j++) {
-            B[i][j] = in.nextInt();
+            uppRight[i][j] = in.nextInt();
          }
       }
+      rows = in.nextInt();
+      cols = in.nextInt();
+      botLeft = new int[rows][cols];
+      for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < cols; j++) {
+            botLeft[i][j] = in.nextInt();
+         }
+      }
+      rows = in.nextInt();
+      cols = in.nextInt();
+      botRight = new int[rows][cols];
+      for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < cols; j++) {
+            botRight[i][j] = in.nextInt();
+         }
+      }
+      System.out.println(botRight[0][0]);
       try { 
-         C = MatrixProduct.matrixProduct_Strassen(A, B);
+         C = MatrixProduct.combQuads(C, uppLeft, uppRight, botLeft, botRight, rows);
          for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < C[0].length; j++) {
                System.out.print(C[i][j] + " ");
             }
             System.out.println();
-         }	
+         }  
       } catch(IllegalArgumentException e) {
          System.err.println("Matrix sizes are not compatible");
       }
